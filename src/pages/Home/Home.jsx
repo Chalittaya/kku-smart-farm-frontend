e69b88a18729ff19615811d58631bb09953626ce
Chalittaya <premie@odds.team>
2022-04-18 01:32:03 +0700

    dashboard / style relay setting

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,21 +1,36 @@
 import "./home.scss";
+import { useEffect, useState } from "react";
 
 const Home = () => {
+  const [items, setItems] = useState([]);
+
+  useEffect(() => {
+    const apiUrl = `http://localhost:9000/dashboard`;
+    fetch(apiUrl)
+      .then((res) => res.json())
+      .then((dashboard) => setItems(dashboard));
+  });
   return (
     <div className="home">
       <div className="home__grid-container">
-        <div className="home__box">
-          <div className="home__item__value">0 %</div>
-          <div className="home__item__name">อุณหภูมิ</div>
-        </div>
-        <div className="home__box">
-          <div className="home__item__value">0 %</div>
-          <div className="home__item__name">ความชื้น</div>
-        </div>
-        <div className="home__box">
-          <div className="home__item__value">0 %</div>
-          <div className="home__item__name">ความชื้นดิน</div>
-        </div>
+        {items.map((item) => {
+          return (
+            <>
+              <div className="home__box">
+                <div className="home__item__value">{item.temperature} °C</div>
+                <div className="home__item__name">อุณหภูมิ</div>
+              </div>
+              <div className="home__box">
+                <div className="home__item__value">{item.humidity} %</div>
+                <div className="home__item__name">ความชื้น</div>
+              </div>
+              <div className="home__box">
+                <div className="home__item__value">{item.soil_moisture} %</div>
+                <div className="home__item__name">ความชื้นดิน</div>
+              </div>
+            </>
+          );
+        })}
       </div>
     </div>
   );
--- src/pages/Relay/RelaySetting.jsx
+++ src/pages/Relay/RelaySetting.jsx
@@ -1,5 +1,7 @@
 import "./relay.scss";
+import Select from "react-select";
 import { useNavigate } from "react-router-dom";
+import TimePicker from "react-time-picker/dist/entry.nostyle";
 
 const RelaySetting = () => {
   let navigate = useNavigate();
@@ -7,15 +9,41 @@ const RelaySetting = () => {
     navigate("/relay");
   };
 
+  const dayPicker = [
+    { label: "อาทิตย์", value: 1 },
+    { label: "จันทร์", value: 2 },
+    { label: "อังคาร", value: 3 },
+    { label: "พุธ", value: 4 },
+    { label: "พฤหัสบดี", value: 5 },
+    { label: "ศุกร์", value: 6 },
+    { label: "เสาร์", value: 7 },
+  ];
+
+  // const minutePicker = [
+  //   { label: "5 นาที", value: 5 },
+  //   { label: "10 นาที", value: 10 },
+  //   { label: "15 นาที", value: 15 },
+  //   { label: "20 นาที", value: 20 },
+  //   { label: "25 นาที", value: 25 },
+  //   { label: "30 นาที", value: 30 },
+  //   { label: "35 นาที", value: 35 },
+  //   { label: "40 นาที", value: 40 },
+  //   { label: "45 นาที", value: 45 },
+  //   { label: "60 นาที", value: 50 },
+  //   { label: "55 นาที", value: 55 },
+  // ];
+
   return (
     <div className="relay__setting">
       <div className="relay__setting__grid">
-        <div className="relay__item__header">ตั้งค่า Wifi</div>
+        <div className="relay__item__header">ตั้งค่ารีเลย์</div>
         <div className="relay__setting__box">
           <div className="relay__item__body">วัน</div>
-          <input type="text" id="day" name="name"></input>
+          <div className="select">
+            <Select options={dayPicker} />
+          </div>
           <div className="relay__item__body">เริ่มทำงาน</div>
-          <input type="text" id="time" name="password"></input>
+          <input type="time" step="1"></input>
           <div className="relay__item__body">ทำงาน (นาที)</div>
           <input type="text" id="minute" name="name"></input>
           <div className="relay__setting__grid">
