c83ddc272dc9d1fbaffa5569025994a9a753ba47
Chalittaya <premie@odds.team>
2022-04-18 00:10:40 +0700

    get wifi api

--- src/pages/Wifi/Wifi.jsx
+++ src/pages/Wifi/Wifi.jsx
@@ -1,17 +1,48 @@
 import "./wifi.scss";
+import { useEffect, useState } from "react";
 
 const Wifi = () => {
+  const [items, setItems] = useState([]);
+
+  useEffect(() => {
+    const apiUrl = `http://localhost:9000/wifi`;
+    fetch(apiUrl)
+      .then((res) => res.json())
+      .then((wifi) => setItems(wifi));
+  });
+
   return (
     <div className="wifi">
       <div className="wifi__grid">
         <div className="wifi__header">ตั้งค่า Wifi</div>
         <div className="wifi__box">
-          <div className="wifi__item">ชื่อ wifi</div>
-          <input type="text" id="name" name="name"></input>
-          <div className="wifi__item">รหัสผ่าน</div>
-          <input type="text" id="password" name="password"></input>
-          <div className="wifi__item">หมายเลขบอร์ด</div>
-          <input type="text" id="boardNo" name="boardNo"></input>
+          {items.map((item) => {
+            return (
+              <>
+                <div className="wifi__item">ชื่อ wifi</div>
+                <input
+                  type="text"
+                  id="name"
+                  name="name"
+                  defaultValue={item.name_wifi}
+                ></input>
+                <div className="wifi__item">รหัสผ่าน</div>
+                <input
+                  type="text"
+                  id="password"
+                  name="password"
+                  defaultValue={item.password_wifi}
+                ></input>
+                <div className="wifi__item">หมายเลขบอร์ด</div>
+                <input
+                  type="text"
+                  id="boardNo"
+                  name="boardNo"
+                  defaultValue={item.num_board}
+                ></input>
+              </>
+            );
+          })}
         </div>
         <button className="wifi__button">บันทึก</button>
       </div>
